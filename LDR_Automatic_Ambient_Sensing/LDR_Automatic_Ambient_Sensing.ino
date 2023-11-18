154e0a55e48e66d2d717b464c4c9a91633da836b
SHARATH V SHETTY <150893698+Sharu1151@users.noreply.github.com>
2023-11-18 13:04:49 +0530

    https://wokwi.com/projects/381443062927724545

---
+++ LDR_Automatic_Ambient_Sensing/LDR_Automatic_Ambient_Sensing.ino
@@ -0,0 +1,13 @@
+const int LDR_PIN = 35; // Define the pin connected to the LDR
+const int LED_PIN = 12; // Define the pin connected to the LED
+void setup() {
+pinMode(LDR_PIN, INPUT);
+pinMode(LED_PIN, OUTPUT);
+}
+void loop() {
+int sensorValue = analogRead(LDR_PIN); // Read the LDR value (0-4095)
+int brightness = map(sensorValue, 0, 4095, 0, 255); // Map to LED brightness
+(0-255);
+analogWrite(LED_PIN, brightness); // Set LED brightness
+delay(100); // Add a small delay for stability
+}
